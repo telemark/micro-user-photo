efb29298498a603a86138a899b5910828824c7f0
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-11-17 22:54:44 +0100

    Switches to mongojs

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const { router, get, post } = require('microrouter')
-const getReadme = require('./lib/get-readme')
-const getPhoto = require('./lib/get-photo')
-const getPhoto64 = require('./lib/get-photo64')
-const uploadPhoto = require('./lib/upload-photo')
+const handlers = require('./lib/handlers')
 const cors = require('micro-cors')()
 
 module.exports = router(
-  get('/', getReadme),
-  get('/user/:user', cors(getPhoto)),
-  get('/user/:user/base64', cors(getPhoto64)),
-  post('/user/:user', cors(uploadPhoto))
+  get('/', handlers.getReadme),
+  get('/user/:user', cors(handlers.getPhoto)),
+  get('/user/:user/base64', cors(handlers.getPhoto64)),
+  post('/user/:user', cors(handlers.uploadPhoto))
 )
