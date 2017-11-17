70b6a1d013e9f0cf2f2aab4c006e819b9e5018d0
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-11-17 23:00:22 +0100

    Updates now

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  server: process.env.DB_SERVER, // with mongodb://, username, password, port, database
-  collection: process.env.DB_COLLECTION,
+  server: process.env.DB_SERVER || 'mongodb://localhost/logs', // with mongodb://, username, password, port, database
+  collection: process.env.DB_COLLECTION || 'images',
   jwt_secret: process.env.JWT_SECRET || 'abc123'
 }
