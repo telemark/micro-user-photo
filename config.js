f6035bf2d9aef58b99e3c1aa2721819685334e49
Jonas Enge <jonas.enge@gmail.com>
2017-11-17 12:53:00 +0100

    genesis commit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   server: process.env.DB_SERVER, // with mongodb://, username, password, port, database
   collection: process.env.DB_COLLECTION,
-  jwt_secret: process.env.JWT_SECRET
+  jwt_secret: process.env.JWT_SECRET || 'abc123'
 }
