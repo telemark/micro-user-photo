ac10ffcf8213b5ce2c13b9aa7f6375bd55da97f7
Jonas Enge <jonas.enge@gmail.com>
2017-11-17 16:23:45 +0100

    genesis commit

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const getReadme = require('./lib/get-readme')
 const getPhoto = require('./lib/get-photo')
 const getPhoto64 = require('./lib/get-photo64')
 const uploadPhoto = require('./lib/upload-photo')
+const cors = require('micro-cors')()
 
 module.exports = router(
   get('/', getReadme),
-  get('/user/:user', getPhoto),
-  get('/user/:user/base64', getPhoto64),
-  post('/user/:user', uploadPhoto)
+  get('/user/:user', cors(getPhoto)),
+  get('/user/:user/base64', cors(getPhoto64)),
+  post('/user/:user', cors(uploadPhoto))
 )
