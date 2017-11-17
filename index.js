b9e3427ace68873a672f8b7df8884f4a631362f3
Jonas Enge <jonas.enge@gmail.com>
2017-11-17 10:29:57 +0100

    genesis commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { router, get } = require('microrouter')
 const getReadme = require('./lib/get-readme')
 const getPhoto = require('./lib/get-photo')
+const getPhoto64 = require('./lib/get-photo64')
 
 module.exports = router(
   get('/', getReadme),
-  get('/user/:user', getPhoto)
+  get('/user/:user', getPhoto),
+  get('/user/:user/base64', getPhoto64)
 )
