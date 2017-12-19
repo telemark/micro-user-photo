11263df1c56de2984c04c23a32e35d31da26d4ad
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-12-19 11:49:31 +0100

    Changes name to SYSTEM_JWT_SECRET (patch)

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   server: process.env.DB_SERVER || 'mongodb://localhost/logs', // with mongodb://, username, password, port, database
   collection: process.env.DB_COLLECTION || 'images',
-  JWT_SECRET: process.env.JWT_SECRET || 'abc123',
+  SYSTEM_JWT_SECRET: process.env.SYSTEM_JWT_SECRET || 'abc123',
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'userphotos',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
   PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345
